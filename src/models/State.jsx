6f995a4df906bd237bec0ec0d6fa6462c718615b
Diwash <diwashdahal75@gmail.com>
2023-02-04 10:42:40 +0545

    feat: add reset method to state

--- src/models/State.jsx
+++ src/models/State.jsx
@@ -1,10 +1,11 @@
 export default class State {
-  constructor(player_id, player_ids, teams, cards, bid_history, played, hands_history, trump_suit = null, trump_revealed = false, all_cards, fully_visible = false) {
+  constructor(player_id, player_ids, teams, cards, bid_history, bid_state, played, hands_history, trump_suit, trump_revealed, all_cards, fully_visible = false) {
     this.player_id = player_id;
     this.player_ids = player_ids;
     this.teams = teams;
     this.cards = cards;
     this.bid_history = bid_history;
+    this.bid_state = bid_state;
     this.played = played;
     this.hands_history = hands_history;
     this.trump_suit = trump_suit;
@@ -12,5 +13,19 @@ export default class State {
     this.all_cards = all_cards;
 
     this.fully_visible = fully_visible;
+  }
+
+  reset() {
+    this.player_id = "You-0";
+    this.player_ids = ["You-0", "Opponent-0", "You-1", "Opponent-1"];
+    this.cards = [];
+    this.bid_history = [];
+    this.bid_state = {"defenderId": "You-0", "challengerId": "Opponent-0", "defenderBid": 0, "challengerBid": 0}
+    this.hands_history = [];
+    this.played = [];
+    this.teams = [{"players": ["You-0", "You-1"], "bid": 0, "won": 0}, {"players": ["Opponent-0", "Opponent-1"], "bid": 0, "won": 0}];
+    this.trump_suit = null
+    this.trump_revealed = false
+    this.all_cards = [[], [], [], []];
   }
 }
