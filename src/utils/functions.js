5dc93cf5331e928cca09f31b5da8d99ce4ca3a27
Diwash <diwashdahal75@gmail.com>
2023-02-04 22:17:54 +0545

    add: util functions

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,57 @@
-const tallyRound = () => {};
+import { getSuitCards, getHandPower, rankCards } from "./utils";
+
+const tallyRound = (state) => {
+  let history = [];
+  let starter = state.hands_history
+    ? state.hands_history.at(-1)[2]
+    : state.player_id;
+  let starter_idx = state.player_ids.indexOf(starter);
+  starter = state.player_ids[starter_idx];
+  history.push(starter);
+  history.push(state.played);
+
+  let played_ranked = rankCards(state.played);
+  let winning_card = None;
+
+  if (!state.trump_revealed) {
+    let curr_suit = state.played[0][1];
+    for (var i = 0, len = played_ranked.length; i < len; i++) {
+      if (played_ranked[i][1] === curr_suit) {
+        winning_card = played_ranked[i];
+        break;
+      }
+    }
+  } else {
+    let trump_played = getSuitCards(played_ranked, state.trump_suit);
+
+    if (!trump_played) {
+      let curr_suit = state.played[0][1];
+      for (var i = 0, len = played_ranked.length; i < len; i++) {
+        if (played_ranked[i][1] === curr_suit) {
+          winning_card = played_ranked[i];
+          break;
+        }
+      }
+    } else {
+      winning_card = trump_played[0];
+    }
+  }
+
+  let points = getHandPower(state.played);
+  let winning_card_idx = state.played.indexOf(winning_card);
+  let winner = state.player_ids[(starter_idx + winning_card_idx) % 4];
+  history.push(winner);
+  state.hands_history.push(history);
+
+  if (state.teams[0]["players"].includes(winner))
+    state.teams[0]["won"] += points;
+  else state.teams[1]["won"] += points;
+
+  state.player_id = winner;
+
+  state.played = [];
+
+  return state;
+};
 
 export default tallyRound;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,21 +1,37 @@
+import { card_rank, powers } from "constants";
+
 const shuffle = function (array) {
   let currentIndex = array.length,
     randomIndex;
-
-  // While there remain elements to shuffle.
   while (currentIndex !== 0) {
-    // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
-
-    // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex],
       array[currentIndex],
     ];
   }
-
   return array;
 };
 
-export default shuffle;
+const getSuitCards = (cards, card_suit) => {
+  return cards.filter((card) => card[1] === card_suit);
+};
+
+const getHandPower = (cards) => {
+  let power = 0;
+  for (let i = 0; i < cards.length; i++) power += powers[cards[i][0]];
+  return power;
+};
+
+const rankCards = (cards) => {
+  let cards_rank = {};
+  for (let i = 0; i < cards.lenght; i++) {
+    cards_rank[cards[i]] = card_rank[cards[i][0]];
+  }
+  return Object.keys(cards_rank).sort(function (a, b) {
+    return list[b] - list[a];
+  });
+};
+
+export default { shuffle, getSuitCards, getHandPower, rankCards };
