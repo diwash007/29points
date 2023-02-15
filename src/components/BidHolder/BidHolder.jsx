ee2912ec264792d3abeb78b314f7fb8de9a71aeb
Diwash <diwashdahal75@gmail.com>
2023-02-15 21:30:38 +0545

    feat: add bidholder

---
+++ src/components/BidHolder/BidHolder.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './BidHolder.css'
+
+function BidHolder() {
+  return (
+    <div>
+      <div className="bid-holder p1-bid"></div>
+      <div className="bid-holder p2-bid"></div>
+      <div className="bid-holder p3-bid"></div>
+      <div className="bid-holder p4-bid"></div>
+    </div>
+  )
+}
+
+export default BidHolder
--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -12,7 +12,8 @@ import GameOver from '../GameOver/GameOver'
 import { cacheImages, dprint } from '../../utils/utils'
 import { ClipLoader } from 'react-spinners'
 import ChooseTrump from '../ChooseTrump/ChooseTrump'
-import ChooseBid from '../ChooseBid/ChooseBid'
+// import ChooseBid from '../ChooseBid/ChooseBid'
+import BidHolder from '../BidHolder/BidHolder'
 
 function Board() {
   const [state, setGameState] = useState(new State())
@@ -88,6 +89,7 @@ function Board() {
       <div className="background">
         <div className="border">
           <div className="table">
+            <BidHolder />
             {isLoading ? (
               <ClipLoader color="white" />
             ) : (
@@ -110,9 +112,9 @@ function Board() {
                   />
                 )}
 
-                {state.playerId === userId && state.bidWinner === null && (
+                {/* {state.playerId === userId && state.bidWinner === null && (
                   <ChooseBid state={state} setGameState={setGameState} />
-                )}
+                )} */}
                 <div className="team2">
                   <Hand
                     cards={state.allCards[1]}
