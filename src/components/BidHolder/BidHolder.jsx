d8f054c26acf2a12cc888d95af3ed2b675beeed5
Diwash <diwashdahal75@gmail.com>
2023-02-16 13:20:51 +0545

    fix: bid holder placements

--- src/components/BidHolder/BidHolder.jsx
+++ src/components/BidHolder/BidHolder.jsx
@@ -4,10 +4,10 @@ import './BidHolder.css'
 function BidHolder() {
   return (
     <div>
-      <div className="bid-holder p1-bid"></div>
-      <div className="bid-holder p2-bid"></div>
-      <div className="bid-holder p3-bid"></div>
-      <div className="bid-holder p4-bid"></div>
+      <div className="bid-holder p1-bid">Bid: 0</div>
+      <div className="bid-holder p2-bid">Bid: 0</div>
+      <div className="bid-holder p3-bid">Bid: 0</div>
+      <div className="bid-holder p4-bid">Bid: 0</div>
     </div>
   )
 }
