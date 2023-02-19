8999f099c0a3c750d24b28c0d47761d70d758644
Diwash <diwashdahal75@gmail.com>
2023-02-19 21:49:52 +0545

    modify: inspiration ribbon + p2 card rotation

--- src/components/Inspiration/Inspiration.jsx
+++ src/components/Inspiration/Inspiration.jsx
@@ -6,10 +6,11 @@ function Inspiration() {
     <a href="https://www.bhoos.com/smartbots/" target="_blank" rel="noreferrer">
       <div id="inspiration">
         <b>Inspiration:</b>
-        <br />
-        LOCUS X BHOOS PRESENTS
-        <br />
-        SMART BOTS 2023
+        <div className="smartbots">
+          LOCUS X BHOOS PRESENTS
+          <br />
+          SMART BOTS 2023
+        </div>
       </div>
     </a>
   )
