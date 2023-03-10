3d03a85a531ceaf21a9c4c73fa8fee1e40fedd7d
Diwash <diwashdahal75@gmail.com>
2023-03-10 12:53:37 +0545

    fix: card img fetch before theme value obtained

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -17,34 +17,36 @@ function Card({ card, index }) {
     roundOver(newState, setGameState)
   }
   return (
-    <img
-      style={{
-        left: `calc(${index} * 20px)`,
-        cursor: card && state.playerId === userId ? 'pointer' : 'default'
-      }}
-      src={`/29points/assets/cards/${theme}/${card ? card + '.png' : 'back.png'}`}
-      className={`card ${
-        card &&
-        state.playerId === userId &&
-        !state.roundOver &&
-        isLegalCard &&
-        state.hiddenTrumpSuit
-          ? 'legal-card'
-          : ''
-      }`}
-      id={card}
-      key={card}
-      alt="playing card"
-      onClick={
-        card &&
-        state.playerId === userId &&
-        !state.roundOver &&
-        isLegalCard &&
-        state.hiddenTrumpSuit
-          ? drawCard
-          : null
-      }
-    />
+    theme && (
+      <img
+        style={{
+          left: `calc(${index} * 20px)`,
+          cursor: card && state.playerId === userId ? 'pointer' : 'default'
+        }}
+        src={`/29points/assets/cards/${theme}/${card ? card + '.png' : 'back.png'}`}
+        className={`card ${
+          card &&
+          state.playerId === userId &&
+          !state.roundOver &&
+          isLegalCard &&
+          state.hiddenTrumpSuit
+            ? 'legal-card'
+            : ''
+        }`}
+        id={card}
+        key={card}
+        alt="playing card"
+        onClick={
+          card &&
+          state.playerId === userId &&
+          !state.roundOver &&
+          isLegalCard &&
+          state.hiddenTrumpSuit
+            ? drawCard
+            : null
+        }
+      />
+    )
   )
 }
 
