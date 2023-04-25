1e0492323725cac323d90e0cfd0c70c9dce0959d
Diwash <diwashdahal75@gmail.com>
2023-04-25 14:08:33 +0545

    fix: show username already exists error

--- src/components/EnterName/EnterName.jsx
+++ src/components/EnterName/EnterName.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './EnterName.css'
 import { useSetUser } from '../../contexts/UserContext'
 import { useDatabase } from '../../contexts/DbContext'
 
 function EnterName() {
+  const [error, setError] = useState()
   const setUser = useSetUser()
   const database = useDatabase()
 
   const handleClick = async () => {
     const username = document.getElementById('username').value
     const { data, error } = await database.from('players').select('name').eq('name', username)
-    if (error || data.length > 0) return
+    if (error || data.length > 0) {
+      setError('Username already in use. Please choose another.')
+      return
+    }
     const { insertError } = await database.from('players').insert({ name: username })
     if (insertError) console.log(insertError)
     setUser(username)
@@ -24,6 +28,7 @@ function EnterName() {
             <input type="text" id="username" required spellCheck="false" />
             <label>Enter name</label>
           </div>
+          <div className="error">{error}</div>
           <br />
           <button className="username-btn" onClick={handleClick}>
             Submit
